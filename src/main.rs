34db8eb0e980865da14010f714dc136d1fd2ed61
RiverMint78 <67481978@qq.com>
2026-02-10 12:11:21 +0800

    feat: 错误采用调试展示

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ async fn main() -> error::Result<()> {
     // 2. 初始化客户端
     let api_client = api::PhilomenaClient::new(cfg)?;
 
-    println!("客户端已就绪，目标站点：{}", api_client.config.base_url);
-
-    // 3. 测试抓取
-    let html = api_client.fetch_home().await?;
-    println!("抓取成功，HTML 长度: {}", html.len());
+    if let Err(e) = api_client.fetch_home().await {
+        eprint!("{:#?}", e);
+    }
 
     Ok(())
 }
