2b1989ab0f7f32ac2a41b44da9908367a82e1b17
RiverMint78 <67481978@qq.com>
2026-02-10 12:19:58 +0800

    chore: play around with match...

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ async fn main() -> error::Result<()> {
     // 1. 加载配置
     let cfg = config::FerrumenaConfig::load();
 
-    // 2. 初始化客户端
-    let api_client = api::PhilomenaClient::new(cfg)?;
+    print!("{:#?}", cfg);
 
-    if let Err(e) = api_client.fetch_home().await {
-        eprint!("{:#?}", e);
+    // 2. 初始化客户端
+    let api_client = match api::PhilomenaClient::new(cfg) {
+        Ok(client) => client,
+        Err(e) => {
+            eprintln!("初始化失败: {:#?}", e);
+            std::process::exit(1);
+        }
+    };
+
+    match api_client.fetch_home().await {
+        Ok(html) => print!("{:#?}", html),
+        Err(e) => eprint!("{:#?}", e),
     }
 
     Ok(())
